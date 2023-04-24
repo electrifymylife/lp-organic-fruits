cf3c1c3b79831c622ebadbafe5294f32232463de
galina <vikhrovagp@gmail.com>
2023-04-25 00:31:58 +0300

    add desktop styling, add icons and libs

---
+++ dist/assets/js/swiper.js
@@ -0,0 +1,1 @@
+const swiper=new Swiper(".swiper",{direction:"horizontal",slidesPerView:1,grabCursor:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
---
+++ src/js/swiper.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  slidesPerView: 1,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
