bb7ad85325fda4cc4d1681bbc854b1195f001f04
galina <vikhrovagp@gmail.com>
2023-04-27 22:19:02 +0300

    add final fixes

--- src/js/smooth-links.js
+++ src/js/smooth-links.js
@@ -1,10 +1,16 @@
+
 window.addEventListener('load', function() {
   const smoothLinks = document.querySelectorAll('a[href^="#"]');
   for (let smoothLink of smoothLinks) {
     smoothLink.addEventListener('click', function (e) {
       e.preventDefault();
       const id = smoothLink.getAttribute('href');
-
+      if (document.body.style.overflow === "hidden") {
+        document.body.removeAttribute("style");
+        navigation.style.left = "-100%";
+        toggle.classList.add("closed");
+        toggle.classList.remove("opened");
+      }
       document.querySelector(id).scrollIntoView({
         behavior: 'smooth',
         block: 'start'
